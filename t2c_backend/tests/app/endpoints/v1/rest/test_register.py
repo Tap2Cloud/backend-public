20dd844defc019b20bc525e99847dd37c1b00dc6
t2c-backend <backend@tap2.cloud>
2026-07-31 12:59:57 +0530

    update test case order by name

--- t2c_backend/tests/app/endpoints/v1/rest/test_register.py
+++ t2c_backend/tests/app/endpoints/v1/rest/test_register.py
@@ -13,7 +13,7 @@ def test_register(client: TestClient, user_data) -> None:
     assert {"refresh_token", "access_token"} <= response.json().keys()
 
 
-@pytest.mark.order(1)
+@pytest.mark.order(after="test_register")
 def test_register_for_second_user(client: TestClient, second_user_data) -> None:
     response = client.post(
         "/api/v1/register",
@@ -24,7 +24,7 @@ def test_register_for_second_user(client: TestClient, second_user_data) -> None:
     assert {"refresh_token", "access_token"} <= response.json().keys()
 
 
-@pytest.mark.order(2)
+@pytest.mark.order(after="test_register_for_second_user")
 def test_register_without_password(client: TestClient, user_data) -> None:
     response = client.post(
         "/api/v1/register", json={"email": user_data["credentials"]["email"], **user_data["basics"]}
@@ -33,7 +33,7 @@ def test_register_without_password(client: TestClient, user_data) -> None:
     assert response.status_code == 422
 
 
-@pytest.mark.order(2)
+@pytest.mark.order(after="test_register_for_second_user")
 def test_register_without_email(client: TestClient, user_data) -> None:
     response = client.post(
         "/api/v1/register",
@@ -43,7 +43,7 @@ def test_register_without_email(client: TestClient, user_data) -> None:
     assert response.status_code == 422
 
 
-@pytest.mark.order(2)
+@pytest.mark.order(after="test_register_for_second_user")
 def test_register_without_firstname_lastname(client: TestClient, user_data) -> None:
     response = client.post("/api/v1/register", json={**user_data["credentials"]})
 
