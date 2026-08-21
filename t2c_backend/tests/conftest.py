fa15c657426eced5564f619d9983acf3247cd3c3
t2c-backend <backend@tap2.cloud>
2026-08-21 14:49:59 +0530

    improve test case

--- t2c_backend/tests/conftest.py
+++ t2c_backend/tests/conftest.py
@@ -104,6 +104,20 @@ def authenticated_client(database_migration, user_data) -> Generator:
         yield c
 
 
+@pytest.fixture(scope="function")
+def second_user_client(authenticated_client, second_user_data) -> Generator:
+    authorization = authenticated_client.headers.pop("Authorization")
+    response = authenticated_client.post(
+        "/api/v1/login",
+        json=second_user_data["credentials"],
+    )
+    assert response.status_code == 200
+    token = response.json()["access_token"]
+    authenticated_client.headers["Authorization"] = f"Bearer {token}"
+    yield authenticated_client
+    authenticated_client.headers.update({"Authorization": authorization})
+
+
 @pytest.fixture(scope="session")
 def asset_type_category_group_container():
     return {}
